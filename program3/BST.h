7d91c47c25b74f451f3a9874256543d9db0c5036
Peter Nguyen <peter.hieu.nguyen@gmail.com>
2016-10-19 21:27:55 -0400

    Program 3: Add documentation

--- program3/BST.h
+++ program3/BST.h
@@ -7,12 +7,20 @@
 #ifndef BST_H
 #define BST_H
 
+/// \class BST
+/// \brief A binary sort tree
 template<typename T>
 class BST {
 public:
+    /// \brief Creates an empty binary sort tree
     BST() : rootPtr(nullptr),
             comparator(defaultCmp) {}
 
+    /// \brief Creates an empty binary sort tree
+    ///
+    /// \param cmp A function that takes two arguments
+    /// and returns true if the left argument is less
+    /// than the right argument
     BST(bool (*cmp)(T, T)) : rootPtr(nullptr),
                              comparator(cmp) {}
 
@@ -20,42 +28,71 @@ public:
         this->clear();
     }
 
+    /// \brief Inserts a new node into the tree
+    ///
+    /// \param key The key of the node
     void insert(const T &key) {
         this->rootPtr = insert(this->rootPtr, key);
     }
 
+    /// \brief Searches the tree for a node
+    ///
+    /// \param key The key of the node
+    /// \return True, if the node exists
     bool find(const T &key) {
         return find(this->rootPtr, key) != nullptr;
     }
 
+    /// \brief Deletes a node from the tree
+    ///
+    /// \param key The key of the node
+    /// \return True, if the node exists
     bool remove(const T &key) {
         const T *const temp = find(this->rootPtr, key);
-        if (temp != nullptr) {
+
+        if (temp != nullptr) { // If the node exists
             remove(this->rootPtr, key);
             return true;
         } else
             return false;
     }
 
+    /// \brief Deletes all the nodes in the tree
     void clear() {
         clear(this->rootPtr);
         this->rootPtr = nullptr;
     }
 
+    /// \brief Pre-order traversal of the tree
+    ///
+    /// \param func This function is called when visiting a node.
     void preorder(void (*func)(T)) {
         preorder(this->rootPtr, func);
     }
 
+    /// \brief In-order traversal of the tree
+    ///
+    /// \param func This function is called when visiting a node.
     void inorder(void (*func)(T)) {
         inorder(this->rootPtr, func);
     }
 
+    /// \brief Post-order traversal of the tree
+    ///
+    /// \param func This function is called when visiting a node.
     void postorder(void (*func)(T)) {
         postorder(this->rootPtr, func);
     }
 
 private:
+    /// \struct Node
+    /// \brief A node in a tree
     struct Node {
+        /// \brief Creates a new node with the provided key
+        ///
+        /// \param key The key of the node
+        /// \param left A pointer to the left node
+        /// \param right A pointer to the right node
         Node(T key, Node *left = nullptr, Node *right = nullptr) :
                 key(key), leftPtr(left), rightPtr(right) {}
 
@@ -64,19 +101,31 @@ private:
         Node *rightPtr;
     };
 
+    /// \brief Recursive helper function to insert a key into a tree
+    ///
+    /// \param root The tree to insert into
+    /// \param key The key to insert
+    /// \return A pointer to the tree with the inserted node
     Node *insert(Node *&root, const T &key) {
+        // Create a new node at the empty position
         if (root == nullptr)
             return new Node(key);
 
         if (comparator(key, root->key)) // key < root->key
-            root->leftPtr = insert(root->leftPtr, key);
+            root->leftPtr = insert(root->leftPtr, key); // Go left
         else                            // key >= root->key
-            root->rightPtr = insert(root->rightPtr, key);
+            root->rightPtr = insert(root->rightPtr, key); // Go right
 
         return root;
     }
 
+    /// \brief Recursive helper function to find a node
+    ///
+    /// \param root The tree to search
+    /// \param key The key to search for
+    /// \return A pointer to the key or nullptr if not found
     const T *const find(Node *root, const T &key) const {
+        // nullptr if node not found
         if (root == nullptr)
             return nullptr;
 
@@ -85,50 +134,74 @@ private:
         else if (comparator(root->key, key)) // key > root->key
             return find(root->rightPtr, key);
         else                                 // key == root->key
-            return &root->key;
+            return &root->key; // Return a pointer to the key
     }
 
+    /// \brief Recursive helper function to delete the minimum node in a tree
+    ///
+    /// \param root The tree to delete from
+    /// \return A pointer to the tree with the minimum-valued node removed
     Node *deleteMin(Node *&root) {
-        if (root->leftPtr == nullptr)
+        if (root->leftPtr == nullptr) // If leftmost node
             return root->rightPtr;
-        else {
+        else { // Keep traversing left
             root->leftPtr = deleteMin(root->leftPtr);
             return root;
         }
     }
 
+    /// \brief Recursive helper function to find the minimum node in a tree
+    ///
+    /// \param root The tree to search
+    /// \return A pointer to the minimum-valued node in the tree
     Node *getMin(Node *&root) {
-        if (root->leftPtr == nullptr)
+        if (root->leftPtr == nullptr) // If leftmost node
             return root;
-        else return getMin(root->leftPtr);
+        else return getMin(root->leftPtr); // Keep traversing left
     }
 
+    /// \brief Recursive helper function to remove a node with the specified key
+    ///
+    /// \param root The tree to remove from
+    /// \param key The key of the node to remove
+    /// \return A pointer to the tree with the node removed
     Node *remove(Node *&root, const T &key) {
-        if (root == nullptr)
+        if (root == nullptr) // The node is not in the tree
             return nullptr;
         else if (comparator(key, root->key)) // key < root->key
             root->leftPtr = remove(root->leftPtr, key);
         else if (comparator(root->key, key)) // key > root->key
             root->rightPtr = remove(root->rightPtr, key);
         else {                               // key == root->key
+            // Found the node to delete
+
+            // Save the pointer to the node
             Node *temp = root;
 
-            if (root->leftPtr == nullptr) {
-                root = root->rightPtr;
-            } else if (root->rightPtr == nullptr)
-                root = root->leftPtr;
+            if (root->leftPtr == nullptr) { // If only right child exists
+                root = root->rightPtr;      // point to right child
+            } else if (root->rightPtr == nullptr) // If only left child exists
+                root = root->leftPtr;             // point to left child
             else {
+                // Get the minimum value in the right subtree
                 temp = getMin(root->rightPtr);
+
+                // Swap the values with the node to delete
                 root->key = temp->key;
                 root->rightPtr = deleteMin(root->rightPtr);
             }
 
+            // Delete the necessary node
             delete temp;
         }
 
         return root;
     }
 
+    /// \brief Recursive helper function to pre-order traverse the tree
+    ///
+    /// \param root Tree to traverse
+    /// \param func Function to call when visiting node
     void preorder(Node *root, void (*func)(T)) {
         if (root == nullptr) return;
         func(root->key);
@@ -136,6 +209,10 @@ private:
         preorder(root->rightPtr, func);
     }
 
+    /// \brief Recursive helper function to in-order traverse the tree
+    ///
+    /// \param root Tree to traverse
+    /// \param func Function to call when visiting node
     void inorder(Node *root, void (*func)(T)) {
         if (root == nullptr) return;
         inorder(root->leftPtr, func);
@@ -143,6 +220,10 @@ private:
         inorder(root->rightPtr, func);
     }
 
+    /// \brief Recursive helper function to post-order traverse the tree
+    ///
+    /// \param root Tree to traverse
+    /// \param func Function to call when visiting node
     void postorder(Node *root, void (*func)(T)) {
         if (root == nullptr) return;
         postorder(root->leftPtr, func);
@@ -150,6 +231,9 @@ private:
         func(root->key);
     }
 
+    /// \brief Recursive helper function delete all nodes in a tree
+    ///
+    /// \param root Tree to delete
     void clear(Node *root) {
         if (root == nullptr) return;
         clear(root->leftPtr);
--- program3/main.cpp
+++ program3/main.cpp
@@ -14,30 +14,66 @@ using namespace std;
 
 const int LINE_LENGTH = 20;
 
-void printValue(const char *value) {
-    cout << value << endl;
+/// \brief Compares two cstrings
+///
+/// \param left Left cstring
+/// \param right Right cstring
+/// \return True, if left occurs before right in lexicographic ordering
+bool cstrcmp(const char *left, const char *right);
+
+/// \brief Prints a menu and prompts user for selection
+///
+/// \return The selection or -1 if an invalid selection
+int promptMenu();
+
+/// \brief Provides respective action based on input
+///
+/// \param tree The tree to mutate
+/// \param input The user selection
+/// \return True, to reprompt the menu
+bool handleInput(BST<const char *> &tree, int input);
+
+/// \brief Prompts the user for a file
+///
+/// \param tree The tree to add keys to
+void insertFromFile(BST<const char *> &tree);
+
+/// \brief Opens the file adds the keys to the tree
+///
+/// \param tree The tree to add keys to
+/// \param filename The file to read
+/// \return -1 if invalid file, else the number of keys added
+int readFile(BST<const char *> &tree, string filename);
+
+/// \brief Prompts the user for a key to add to the tree
+///
+/// \param tree The tree to add the key to
+void insertFromKeyboard(BST<const char *> &tree);
+
+/// \brief Prompts the user for a key
+/// and searches the tree for the key
+///
+/// \param tree The tree to search
+void findKey(BST<const char *> &tree);
+
+/// \brief Prompts the user for a key
+/// and attempts to delete the key
+///
+/// \param tree The tree to remove the key from
+void deleteKey(BST<const char *> &tree);
+
+/// \brief Prints the cstring
+///
+/// \param value The cstring to print
+void printValue(const char *value);
+
+int main() {
+    BST<const char *> tree(&cstrcmp);
+    while (handleInput(tree, promptMenu()));
 }
 
 bool cstrcmp(const char *left, const char *right) {
     return strcmp(left, right) < 0;
-}
-
-int readFile(BST<const char *> &tree, string filename) {
-    ifstream infile;
-    infile.open(filename);
-    if (infile.fail()) return -1;
-
-    unsigned lines = 0;
-
-    for (char line[LINE_LENGTH + 1];
-         !infile.getline(line, LINE_LENGTH + 1).eof();) {
-        tree.insert(strdup(line));
-        ++lines;
-    }
-
-    infile.close();
-
-    return lines;
 }
 
 int promptMenu() {
@@ -60,6 +96,60 @@ int promptMenu() {
     else return input;
 }
 
+bool handleInput(BST<const char *> &tree, int input) {
+    switch (input) {
+        case 1:
+            cout << endl;
+            insertFromFile(tree);
+            break;
+        case 2:
+            cout << endl;
+            insertFromKeyboard(tree);
+            break;
+        case 3:
+            cout << endl;
+            findKey(tree);
+            break;
+        case 4:
+            cout << endl;
+            deleteKey(tree);
+            break;
+        case 5:
+            cout << "\033[1;33m";
+            cout << "\n------------------------------\n\n";
+            tree.inorder(printValue);
+            cout << "\n------------------------------\n";
+            cout << "\033[0m";
+            break;
+        case 6:
+            cout << "\033[1;33m";
+            cout << "\n------------------------------\n\n";
+            tree.preorder(printValue);
+            cout << "\n------------------------------\n";
+            cout << "\033[0m";
+            break;
+        case 7:
+            cout << "\033[1;33m";
+            cout << "\n------------------------------\n\n";
+            tree.postorder(printValue);
+            cout << "\n------------------------------\n";
+            cout << "\033[0m";
+            break;
+        case 8:
+            return false;
+        default:
+            cin.clear();
+            cin.ignore();
+            cout << "\n\033[1;31m" <<
+                 "Invalid input!\n" <<
+                 "Please enter a number between 1 and 8." <<
+                 "\033[0m\n";
+            break;
+    }
+
+    return true;
+}
+
 void insertFromFile(BST<const char *> &tree) {
     string filepath;
     cout << "Enter the file path: ";
@@ -75,6 +165,24 @@ void insertFromFile(BST<const char *> &tree) {
         cout << "\n\033[1;32m"
              << "Read " << result << " lines from file: " << filepath
              << "\033[0m\n";
+}
+
+int readFile(BST<const char *> &tree, string filename) {
+    ifstream infile;
+    infile.open(filename);
+    if (infile.fail()) return -1;
+
+    unsigned lines = 0;
+
+    for (char line[LINE_LENGTH + 1];
+         !infile.getline(line, LINE_LENGTH + 1).eof();) {
+        tree.insert(strdup(line));
+        ++lines;
+    }
+
+    infile.close();
+
+    return lines;
 }
 
 void insertFromKeyboard(BST<const char *> &tree) {
@@ -141,61 +249,6 @@ void deleteKey(BST<const char *> &tree) {
     }
 }
 
-bool handleInput(BST<const char *> &tree, int input) {
-    switch (input) {
-        case 1:
-            cout << endl;
-            insertFromFile(tree);
-            break;
-        case 2:
-            cout << endl;
-            insertFromKeyboard(tree);
-            break;
-        case 3:
-            cout << endl;
-            findKey(tree);
-            break;
-        case 4:
-            cout << endl;
-            deleteKey(tree);
-            break;
-        case 5:
-            cout << "\033[1;33m";
-            cout << "\n------------------------------\n\n";
-            tree.inorder(printValue);
-            cout << "\n------------------------------\n";
-            cout << "\033[0m";
-            break;
-        case 6:
-            cout << "\033[1;33m";
-            cout << "\n------------------------------\n\n";
-            tree.preorder(printValue);
-            cout << "\n------------------------------\n";
-            cout << "\033[0m";
-            break;
-        case 7:
-            cout << "\033[1;33m";
-            cout << "\n------------------------------\n\n";
-            tree.postorder(printValue);
-            cout << "\n------------------------------\n";
-            cout << "\033[0m";
-            break;
-        case 8:
-            return false;
-        default:
-            cin.clear();
-            cin.ignore();
-            cout << "\n\033[1;31m" <<
-                 "Invalid input!\n" <<
-                 "Please enter a number between 1 and 8." <<
-                 "\033[0m\n";
-            break;
-    }
-
-    return true;
-}
-
-int main() {
-    BST<const char *> tree(&cstrcmp);
-    while (handleInput(tree, promptMenu()));
+void printValue(const char *value) {
+    cout << value << endl;
 }
